c3ddc72679131087fd131701451541f1f815698a
mathieu <m.escos@it-students.fr>
2021-04-01 13:53:03 +0200

    final commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,30 +15,74 @@ class UserController extends Controller
     
     public function index(Request $request)
     {
-        if ($request->has('username')) {
-            $user = User::all()->where('username', $request->get('username'))->first();
-            return view('user', [
-                'user' => $user,
-                'hwacks' => Hwack::where('user_id', $user->id, 'private', false)->latest('created_at')->simplePaginate(100),
-            ]);
-        }
 
-        $user = auth()->user();
+        $user = User::all()->where('username', $request->get('username'))->first();
 
-        if (!$user) {
+        // If user does not exist redirect to user page
+        if ($user) {
+            // Do nothing
+        } elseif($request->has('username')){
             return redirect()->route('user');
         }
 
+        // ?username=mathieu
+        if ($request->has('username')) {
+            $hwacks = Hwack::where(
+                [
+                    ['user_id', '=', $user->id],
+
+                ])
+                ->latest('created_at')->simplePaginate(100);
+        } else {
+            // /user/
+            $user = auth()->user();
+            $hwacks = Hwack::where('private', false)->latest('created_at')->simplePaginate(100);
+        }
+        
+        // ?username= && private == true Others username
+        if ($request->has('username') && $request->has('private') && $request->get('private') == 'true') {
+            $hwacks = Hwack::where(
+                [
+                    ['user_id', '=', $user->id],
+                    ['private', '=', true],
+                ]
+            )->latest('created_at')->simplePaginate(100);
+        }
+        
+        // ?username= && private == false Others username
+        if ($request->has('username') && $request->get('private') == 'false') {
+            $hwacks = Hwack::where(
+                [
+                    ['user_id', '=', $user->id],
+                    ['private', '=', false]
+                ]
+            )->latest('created_at')->simplePaginate(100);
+        }
+
+        // ?search= && private == false
+        if($request->get('search')) {
+            $search = $request->get('search');
+            $hwacks = Hwack::where(
+                [
+                    ['private', '=', 'false'],
+                    ['content', '=', "$search"],
+                ]
+            
+            )->latest('created_at')->simplePaginate(100);
+        }
+
+        
+
         return view('user', [
             'user' => $user,
-            'hwacks' => Hwack::where('private', false)->latest('created_at')->simplePaginate(100),
+            'hwacks' => $hwacks,
         ]);
     }
 
     public function createHwack(Request $request)
     {
         $request->validate([
-            'image' => 'required|mimes:jpg,png,gif,webp',
+            'image' => 'mimes:jpg,png,gif,webp',
             'content' => 'required|max:500',
             'private' => 'nullable',
         ]);
